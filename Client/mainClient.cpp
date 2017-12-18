f249c393971f32e275d496b5874134620d649693
Felipe <felipefuhr7@gmail.com>
2017-12-18 18:14:23 -0200

    resolvi voltar temporariamente a um modelo proxy != cliente. melhor deixar esse problema para depois

--- Client/mainClient.cpp
+++ Client/mainClient.cpp
@@ -13,9 +13,8 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "dropboxClient.h"
-#include "clientProxy.h"
 
-int client_process(int argc, char** argv){
+int main(int argc, char** argv){
     pthread_t fileWatcherThread;
     char comand[MAXCOMANDSIZE], syncDirPath[256];
     int isRunning = 1;
@@ -85,48 +84,4 @@ int client_process(int argc, char** argv){
                 break;
         }
     }
-}
-
-int proxy_process(){
-    ClientProxy proxy;
-    int isRunning=1;
-
-    //Inicialização do socket de comunicação com o cliente
-    if(proxy.initialize_clientConnection() < 0){
-        return -1;
-    }
-
-    /// TODO: Conectar a vários servidores, gerenciar mensagens dadas por eles (possivelmente tudo isso tem de ser transferido para o while abaixo)
-
-    // Conecta ao servidor TODO: trocar argumentos para um txt, com lista de servidores
-    if(proxy.connect_server("localhost", 4000) < 0){
-        return -1;
-    }
-    ///////////////////////////////////////////////////////////
-
-    fprintf(stderr, "Server is listening.\n");
-
-
-    // TODO: if checkServer == 1 else abaixo
-    // Recebe informação do Cliente e manda ao Servidor e vice-versa
-    pthread_t *clientThread;
-    pthread_t *serverThread;
-
-    proxy.set_communicationSocket(proxy.listenAndAccept());
-
-    clientThread = proxy.clientWatcher();
-    serverThread = proxy.serverWatcher();
-
-    pthread_join(*clientThread, NULL);
-    pthread_join(*serverThread, NULL);
-
-    close(proxy.get_clientSocket());
-    return 0;
-}
-
-int main(int argc, char** argv){
-    int pid = fork();
-    if(pid < 0) fprintf(stderr, "%s\n", "Error! Couldn't open Client.");
-    else if(pid == 0) client_process(argc, argv);
-    else proxy_process();
 }
